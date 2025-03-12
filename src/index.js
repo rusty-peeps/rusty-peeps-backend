43861a3ed0e4ddefa7eeb2b78fecfd26a22bc839
NishikantaRay <nishikantaray1@gmail.com>
2025-03-12 22:53:16 +0530

    package update

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(`/api/${api_version}`, routes());
 
+
 app.listen(port, () => {
   logger.info(`Server started at ${host}:${port}/api/${api_version}/`);
 });
