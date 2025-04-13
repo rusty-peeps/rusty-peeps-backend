fd16f344c3c5f8c9453312ca1ca6015840d23283
NishikantaRay <nishikantaray1@gmail.com>
2025-04-13 23:33:40 +0530

    push

--- src/routes/newsletter.js
+++ src/routes/newsletter.js
@@ -7,31 +7,24 @@ import { createNewsletter } from "../validators/joi.validator.js";
 router.post("/newsletter", createNewsletter, async (req, res) => {
   try {
     const { email } = req.body;
+    let isEmailExist = await NewsLetter.findOne({ email });
+    if (isEmailExist) {
+      return res.status(201).json({
+        message: "Email already exists",
+        code: 201,
+      });
+    }
     const newNewsLetter = new NewsLetter({
       email,
     });
     await newNewsLetter.save();
-    res.status(201).json({
-      data: newNewsLetter,
-      message: "Successfully created newsletter",
-      code: 201,
-    });
-  } catch (err) {
-    console.error("Error updating slot:", err);
-    res.status(500).json({ error: err.message });
-  }
-});
-
-router.get("/newsletter", async (req, res) => {
-  try {
-    const newsletters = await NewsLetter.find();
     res.status(200).json({
-      data: newsletters,
-      message: "Successfully fetched all newsletters",
+      data: newNewsLetter,
+      message: "Successfully created.",
       code: 200,
     });
   } catch (err) {
-    res.status(500).json({ status: "failed", error: err.message });
+    res.status(500).json({ error: err.message });
   }
 });
 
